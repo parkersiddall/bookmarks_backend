ee141579c75c46ae51487ae0499d3c3c93849bf0
Parker Siddall <parker.siddall@gmail.com>
2021-07-04 10:46:46 +0200

    Adjusted reddit scraper to return only posts with images.

--- utils/redditScraper.js
+++ utils/redditScraper.js
@@ -1,17 +1,53 @@
 const axios = require('axios')
 
 const scrapeReddit = async (subreddit, limit) => {
-  // function that calls Reddits open API given a the subreddir and a search limit. 
-  const topPosts = await axios.get(`https://www.reddit.com/r/${subreddit}/top.json?limit=${limit}`)
+  // function that calls Reddits open API given a the subreddir and a search limit.
+  let currentPost = null 
+  let successful = []
+  let unsuccessful = 0
 
-  const topPostsCleaned = topPosts.data.data.children.map(post => {
-    return {
+  let totalCalls = 0
+
+  // make an initial call
+  const response = await axios.get(`https://www.reddit.com/r/${subreddit}/new.json?limit=${1}`)
+  const post = response.data.data.children[0]
+  
+  currentPost = post.data.name
+  totalCalls++
+  
+  if (post.data.post_hint === 'image') {
+    savedPost = {
       title: post.data.title,
       url: post.data.url
     }
-  })
+    
+    successful.push(savedPost)
+  }
+  
+  while (successful.length < limit) {
+    console.log(currentPost)
+    const response = await axios.get(`https://www.reddit.com/r/${subreddit}/new.json?limit=${1}&after=${currentPost}`)
+    totalCalls++
+    
+    const post = response.data.data.children[0]
+    currentPost = post.data.name
 
-  return topPostsCleaned
+    if (post.data.post_hint === 'image') {
+      savedPost = {
+        title: post.data.title,
+        url: post.data.url
+      }
+
+      successful.push(savedPost)
+    } else {
+      unsuccessful++
+    }
+  }
+
+  console.log('Total calls: ', totalCalls)
+  console.log('Unsuccessful: ', unsuccessful)
+
+  return successful
 }
 
 const getPostForNewBookmark = async (subreddit, limit) => {
