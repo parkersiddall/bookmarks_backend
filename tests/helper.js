2f2a8a456aadd0b2185a74e72d133906800348a5
Parker Siddall <parker.siddall@gmail.com>
2021-07-10 16:23:08 +0200

    Wrote test for creating a new user account

---
+++ tests/helper.js
@@ -0,0 +1,11 @@
+// will add some initial bookmarks, users, and functions in here to 
+// keep my test files clean
+
+const testUser = {
+  username: "Nic Cage",
+  password: "independence_1776"
+}
+
+module.exports = {
+  testUser
+}
---
+++ tests/users.test.js
@@ -0,0 +1,54 @@
+const app = require('../app')
+const mongoose = require('mongoose')
+const helper = require('./helper')
+const supertest = require('supertest')
+const User = require('../models/user')
+
+const api = supertest(app)
+
+beforeEach(async () => {
+  // clear test database of users
+  await User.deleteMany({})
+})
+
+describe('Creating an account', () => {
+  test('Users get 200 after submitting form', async () => {
+    const response = await api
+      .post('/api/users')
+      .send(helper.testUser)
+  
+    expect(response.status === 200)
+  })
+
+  test('Response contains correct information about created account', async () => {
+    const response = await api
+      .post('/api/users')
+      .send(helper.testUser)
+
+    expect(response.body.username === helper.testUser.username)
+    expect(response.body.subreddit === 'earthporn')
+    expect(response.body.prefersDark === false)
+    expect(response.body.bookmarks).toHaveLength(0)
+    expect(response.body.savedPhotos).toHaveLength(0)
+  })
+
+  test('DB contains correct number of users', async () => {
+    // before registering new user
+    const users = await User.find()
+    expect(users.length === 0)
+
+    // create new user
+    const response = await api
+      .post('/api/users')
+      .send(helper.testUser)
+
+    // after registering new user
+    const newUsers = await User.find()
+    expect(newUsers.length === 1)
+  })
+})
+
+// close DB connection
+afterAll(() => {
+  mongoose.connection.close()
+})
